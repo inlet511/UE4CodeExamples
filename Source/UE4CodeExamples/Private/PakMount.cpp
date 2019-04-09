3fc5597c42cba4082cfc23048bc9670da8233436
inlet511 <inlet511@gmail.com>
2019-04-09 14:54:13 +0800

    WIP

--- Source/UE4CodeExamples/Private/PakMount.cpp
+++ Source/UE4CodeExamples/Private/PakMount.cpp
@@ -25,48 +25,78 @@ void APakMount::Tick(float DeltaTime)
 
 void APakMount::MountPak()
 {
-    //第一步
-    //FPlatformFileManager::Get()返回单例
-    //GetPlatformFile()返回相应平台的PlatformFile，即处理相应平台文件读写的对象
-    //因此在Windows平台，这里返回的是FWindowsPlatformFile的实例
+	//第一步
+	//FPlatformFileManager::Get()返回单例
+	//GetPlatformFile()返回相应平台的PlatformFile，即处理相应平台文件读写的对象
+	//因此在Windows平台，这里返回的是FWindowsPlatformFile的实例
 	IPlatformFile& InnerPlatform = FPlatformFileManager::Get().GetPlatformFile();
 
-    //第二步
-    //这里创建了一个FPakPlatformFile，但是未指定当前使用什么平台去读写这个文件
+	//第二步
+	//这里创建了一个FPakPlatformFile，但是未指定当前使用什么平台去读写这个文件
 	FPakPlatformFile* PakPlatformFile = new FPakPlatformFile();
 
-    //第三步
-    //使用相应平台的PlatformFile去初始化PakPlatformFile
-    //第二个参数是命令行参数，一般都为空
+	//第三步
+	//使用相应平台的PlatformFile去初始化PakPlatformFile
+	//第二个参数是命令行参数，一般都为空
 	PakPlatformFile->Initialize(&InnerPlatform, TEXT(""));
 
-    //第四步
-    //再将当前PlatformFile设置为"相应平台下pak文件读写"的模式
+	//第四步
+	//再将当前PlatformFile设置为"相应平台下pak文件读写"的模式
 	FPlatformFileManager::Get().SetPlatformFile(*PakPlatformFile);
-	
-
-    const FString pakFilename = TEXT("D:\\ExamplePak.pak");
 
 
-    TSharedPtr<IFileHandle> PakHandle = MakeShareable(InnerPlatform.OpenRead(*pakFilename));
+	const FString pakFilename = TEXT("D:\\ExamplePak.pak");
+
+
+	TSharedPtr<IFileHandle> PakHandle = MakeShareable(InnerPlatform.OpenRead(*pakFilename));
 	if (!PakHandle.IsValid())
-        return;
+		return;
 
-    //创建FPakFile对象,同样使用相应平台的PlatformFile初始化
-    //第二个参数是pak文件名称
-    //第三个参数是是否有符号？
-    FPakFile* Pak = new FPakFile(&InnerPlatform,*pakFilename, false);
-
-    //具体Mount方法可以参考函数 FPakPlatformFile::Mount
-    //但是其中有大量多余内容(例如版本编号处理),可以调用其中最核心的部分SetMountPoint
-    Pak->SetMountPoint(*FPaths::EngineContentDir());
+	//创建FPakFile对象,同样使用相应平台的PlatformFile初始化
+	//第二个参数是pak文件名称
+	//第三个参数是是否有符号？
+	FPakFile* Pak = new FPakFile(&InnerPlatform, *pakFilename, false);
 
 
-    //bool bResult = PakPlatformFile->Mount(*FString(TEXT("D:\\ExamplePak.pak")), PakOrder, *FPaths::EngineContentDir());
+	if (Pak->IsValid())
+	{
+		//具体Mount方法可以参考函数 FPakPlatformFile::Mount
+		//但是其中有大量多余内容(例如版本编号处理),可以调用其中最核心的部分SetMountPoint
+		Pak->SetMountPoint(*FPaths::EngineContentDir());
+
+		class Dump : public IPlatformFile::FDirectoryVisitor
+		{
+            public:
+			const FString mp_PakName;
+			TArray<FString> Files;
+
+			Dump(const FString& PakfileName) :
+				mp_PakName(PakfileName)
+			{
+			}
+			virtual bool Visit(const TCHAR* FilenameOrDirectory, bool bIsDirectory)
+			{
+				UE_LOG(LogTemp, Log, TEXT("%s"), FilenameOrDirectory);
+				return true;
+			}
+		};
+
+		Dump* Visitor = new Dump(pakFilename);
+
+
+		if (FCoreDelegates::OnMountPak.Execute(pakFilename, 0, Visitor))
+		{
+			TArray<FString> Files;
+			Pak->FindFilesAtPath(Files, *FPaths::EngineContentDir(), true, false, true);
+		}
+	}
+
+
+	//bool bResult = PakPlatformFile->Mount(*FString(TEXT("D:\\ExamplePak.pak")), PakOrder, *FPaths::EngineContentDir());
 
 	// if (bResult)
 	// {
 	// 	GEngine->AddOnScreenDebugMessage(-1, 50.f, FColor::Red, FString::Printf(TEXT("EngineContentDir '%s'"), *FPaths::EngineContentDir()));
 
 	// }
-} 
+}
